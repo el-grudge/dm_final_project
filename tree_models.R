8d85642deb7fb358b502e4918ddf6fa50b6cd68b
minasonbol@gmail.com <minasonbol@gmail.com>
2020-04-20 14:41:45 -0400

    tree models, working script v1

--- tree_models.R
+++ tree_models.R
@@ -2,60 +2,24 @@
 # 0,1 are interpreted as integers, creating a regression tree instead of a classification tree
 df_phy <- select(data,-exampleid)
 df_phy$phy_target <- factor(ifelse(df_phy$target==0,'No','Yes'))
-df_phy <- select(df_phy, -target)
+df_phy <- select(df_phy, phy_target, everything(), -target)
 
-# splitting the datasets
+# splitting the datasets to train (75%) & test (25%)
 set.seed(2)
 train_phy=sample(1:nrow(df_phy), 0.75*nrow(df_phy))
 test_phy=df_phy[-train_phy,]
 test_target=df_phy$phy_target[-train_phy]
 
-
-# # basic tree building, with evaluation
-# tree_phy=tree(phy_target~.,df_phy,subset=train_phy)
-# tree_pred=predict(tree_phy,test_phy,type='class')
-# table(tree_pred,test_target)
-# (4020+4321)/(4020+1881+2278+4321)
-# 
-# summary(tree_phy)
-# plot(tree_phy)
-# text(tree_phy,pretty=0)
-# 
-# # considering pruning the tree
-# # size -> number of terminal nodes
-# # dev -> cross.validation error
-# # k -> cost complexity parameter
-# set.seed(3)
-# cv_phy=cv.tree(tree_phy, FUN=prune.misclass)
-# names(cv_phy)
-# cv_phy
-# 
-# # plot error rate as a function of size & k
-# par(mfrow=c(1,2))
-# plot(cv_phy$size, cv_phy$dev, type="b")
-# plot(cv_phy$k, cv_phy$dev, type="b")
-# 
-# # prouning the tree
-# prune_phy=prune.misclass(tree_phy, best=2)
-# plot(prune_phy)
-# text(prune_phy)
-# 
-# # measuring predictive performance of pruned tree
-# test_phy_pruned=predict(prune_phy, test_phy, type='class')
-# table(test_phy_pruned, test_target)
-# (4020+4321)/(4020+1881+2278+4321) # a tree of size 2 has performs similar to a tree of size 5
-
 ############################################################
-# bagging and random forest
+# Random forest
 # randomforest, uses less variables so mtry should be smaller
 # mtry (number of featuers considered by model): for regression p=p/3
 # mtry (number of featuers considered by model): for classification p=sqrt(p)
-#library(randomForest)
 set.seed(1)
 rf_phy=randomForest(phy_target~.,
                     data=df_phy,
-                    subset=train_phy, 
-                    mtry=sqrt(length(colnames(df_phy))), 
+                    subset=train_phy,
+                    mtry=sqrt(length(colnames(df_phy))),
                     importance=TRUE)
 phy_target_rf=predict(rf_phy, newdata=df_phy[-train_phy,])
 table(phy_target_rf, test_target)
@@ -63,14 +27,11 @@ table(phy_target_rf, test_target)
 
 # importance to see importance of each variable
 importance(rf_phy)
- 
+
 # plotting importance
 varImpPlot(rf_phy)
- 
-
 ############################################################
 # boosting
-#library(gbm)
 set.seed(1)
 
 # use distribution='gaussian' for regression
@@ -81,7 +42,7 @@ set.seed(1)
 boost_phy=gbm(phy_target~.,
               data=df_phy[train_phy,],
               distribution='multinomial',
-              n.trees=5000, 
+              n.trees=100, 
               interaction.depth=4)
 boost_phy_summ <- summary(boost_phy)
 
@@ -94,20 +55,27 @@ plot(boost_phy,i='feat14')
 # using boosted model for prediction
 phy_target_boost=predict(boost_phy, 
                          newdata = df_phy[-train_phy,], 
-                         n.trees=5000)
+                         n.trees=100,
+                         type='response')
+
+phy_target_boost = colnames(phy_target_boost)[apply(phy_target_boost, 1, which.max)]
 table(phy_target_boost, test_target)
+(4648+4418)/(4648+1786+1650+4416) # slightly better performance of 72.5 %
 
 # using different value for shrinkage parameter
 boost_phy_2 = gbm(phy_target~.,
                   data=df_phy[train_phy,],
                   distribution='multinomial',
-                  n.trees=5000, 
+                  n.trees=100, 
                   interaction.depth = 4,
                   shrinkage = 0.2,
                   verbose=F)
 
 phy_target_boost2=predict(boost_phy_2, 
                          newdata = df_phy[-train_phy,], 
-                         n.trees=5000)
+                         n.trees=100,
+                         type='response')
 
+phy_target_boost2 = colnames(phy_target_boost2)[apply(phy_target_boost2, 1, which.max)]
 table(phy_target_boost2, test_target)
+(4584+4401)/(4584+1801+1714+4401) # 71.8 % increasing shrinkage parameter has a detrimental effect on performance
